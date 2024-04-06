a7df472953c76faf8a71ec52f4033fa347dca95c
UsmanalievI <uin.2201@gmail.com>
2024-04-06 19:59:54 +0600

    exam

---
+++ examDay/src/pages/mainPage/MainPage.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { Container } from '../../components/Container'
+import { Button } from '../../components/Button'
+import { useDispatch } from 'react-redux'
+import { getPosts } from '../../redux/PostSlice'
+
+function MainPage() {
+    const dispatch=useDispatch()
+
+    const [user, setUser]=useState({
+        email:'',
+        password:'',
+        repeatPassword:''
+    })
+
+    const regExp=/^[A-Za-z0-9_+-.]+@gmail\.com$/;
+
+    const changeInput=(event)=>{
+        setUser({
+            ...user,
+            [event.target.name]:event.target.value
+        })
+    }
+    
+    const addUser=()=>{
+        if(regExp.test(user.email) ||user.password===user.repeatPassword){
+            dispatch(getPosts(user))
+        }else{
+            alert('введенные данные неправильны, перевроверьте')
+        }
+    }
+    
+    
+
+  return (
+    <Container variant='small'>
+        <div style={{display: 'flex', justifyContent:'space-between', margin:'30px auto'}}>
+            <input
+                name='E-Mail'
+                label='Enter g-Mail'
+                placeholder='g-Mail'
+                onChange={changeInput}
+            />
+            <input
+                name='password'
+                label='Enter Password'
+                placeholder='Password'
+                type='password'
+                onChange={changeInput}
+            />
+            <input
+                name='repeatPassword'
+                label='Repeat Password'
+                placeholder='repeat Password'
+                type='password'
+                onChange={changeInput}
+            />
+            <Button onClick={addUser} variant='primary'>Sign Up</Button> 
+        </div>
+    </Container>
+  )
+}
+
+export default MainPage
---
+++ examDay/src/redux/PostSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getPosts=createAsyncThunk(
+    'getPosts',
+    async function (info, {dispatch}){
+        const options={
+            method: "POST",
+            headers:{
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(info)
+        }
+        const response=await fetch('https://jsonplaceholder.typicode.com/posts', options)
+        const data= await response.json()
+        dispatch(postsInfoPosts(data))
+    }
+)
+
+const postsSlice=createSlice({
+    name: 'postsSlice',
+    initialState:{
+        user: []
+    },
+    reducers: {
+        postsInfoPosts:(state, action)=>{
+            state.posts=action.payload
+        }
+    }
+})
+
+export const { postsInfoPosts}=postsSlice.actions
+
+export default postsSlice.reducer
